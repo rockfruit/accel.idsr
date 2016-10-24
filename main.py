49518556e4352a8f4bf34854436c619c0363f2f5
Campbell <campbell@bikalabs.com>
2016-10-24 21:36:31 +0200

    Added jquery.steps.js library and added static reference

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
+static('jquery-steps.min.js', filename='jquery-steps.min.js')
+
 @app.route('/')
-def hello():
-    return ''
+def index():
+    return render_template('index.html')
 
 @app.route('/idsr-input')
 def idsr_input():
