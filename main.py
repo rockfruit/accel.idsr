cdec4edac2ed2bcd300b38d568a2209c5ecbbe43
Campbell <campbell@bikalabs.com>
2016-10-25 20:45:24 +0200

    Basic structure of nested step-forms implemented.
    There are issues, but we can start working on the different sections
    now.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import render_template
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
-
-static('jquery-steps.min.js', filename='jquery-steps.min.js')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/idsr-input')
-def idsr_input():
-    return render_template('idsr-input.html')
-
+@app.route('/idsr-input-minimal')
+def idsr_input_minimal():
+    return render_template('idsr-input-minimal.html')
