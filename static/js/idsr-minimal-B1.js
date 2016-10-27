75f6bc60b7eb69bb5e850c0359c1bcbf5dbbdfd4
austinsdoe <austinsdoe@gmail.com>
2016-10-27 13:19:46 -0400

    Fixed Bad Merge

--- static/js/idsr-minimal-B1.js
+++ static/js/idsr-minimal-B1.js
@@ -2,10 +2,6 @@
 (function($) {
     $(document).ready(function() {
 
-        $("input#other_disease").hide();
-        $("input#other").click(function () {
-            $("input#other_disease").show();
-        });
 
     })
 })(jQuery);
