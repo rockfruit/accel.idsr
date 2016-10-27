41aebf4be8b7bef0d97270d1a9f2343138a3894a
austinsdoe <austinsdoe@gmail.com>
2016-10-27 13:56:40 -0400

    Review fixes

--- static/js/idsr-minimal-B1.js
+++ static/js/idsr-minimal-B1.js
@@ -1,13 +1,13 @@
 (function ($) {
     $(document).ready(function () {
 
-        $("#other_disease").hide();
+        $("#B1 #other_disease").hide();
 
-        $("#other").click(function () {
-            $("#other_disease").show();
+        $("#B1 #other").click(function () {
+            $("#B1 #other_disease").show();
         });
-        $(".idsr_disease").click(function () {
-            $("#other_disease").hide();
+        $("#B1 .idsr_disease").click(function () {
+            $("#B1 #other_disease").hide();
         });
 
         $("#B1 input").checkboxradio();
